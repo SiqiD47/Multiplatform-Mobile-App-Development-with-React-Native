338e71d5fc607edb08a4003107c7e31a6892b19a
siqidai97@gmail.com <siqidai97@gmail.com>
2020-06-28 18:03:15 -0400

    Navigation Part 1

--- ReactNative/confusion/components/DishdetailComponent.js
+++ ReactNative/confusion/components/DishdetailComponent.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function RenderDish(props) {
+function RenderDish({dish}) {
 
-    const dish = props.dish;
-    
+    //const dish = props.dish;  
         if (dish != null) {
             return(
                 <Card
@@ -22,8 +22,25 @@ function RenderDish(props) {
         }
 }
 
-function Dishdetail(props) {
-    return(<RenderDish dish={props.dish} />);
+class Dishdetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId','');
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
+    }
 }
 
 export default Dishdetail;
