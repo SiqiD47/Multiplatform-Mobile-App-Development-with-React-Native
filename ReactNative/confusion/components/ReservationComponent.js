b255bda7b77e9da0163dd91463b52e4c398360ee
siqidai97@gmail.com <siqidai97@gmail.com>
2020-07-01 18:56:00 -0400

    Local Notifications

--- ReactNative/confusion/components/ReservationComponent.js
+++ ReactNative/confusion/components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, Modal, ScrollView, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
 
@@ -30,7 +32,7 @@ class Reservation extends Component {
             'Number of Guests: ' + this.state.guests + '\nSmoking? ' + this.state.smoking + '\nDate and Time: '+ this.state.date,
             [
             {text: 'Cancel', onPress: () => this.resetForm(), style: 'cancel' },
-            {text: 'OK', onPress: () => { console.log(JSON.stringify(this.state)); this.resetForm() }},
+            {text: 'OK', onPress: () => { console.log(JSON.stringify(this.state)); this.presentLocalNotification(this.state.date); this.resetForm() }},
             ],
             { cancelable: false }
         );
@@ -44,6 +46,33 @@ class Reservation extends Component {
             smoking: false,
             date: '',
             //showModal: false
+        });
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
         });
     }
     
